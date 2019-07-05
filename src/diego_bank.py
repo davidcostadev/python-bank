776093d1713bb2a6e894c9f893935946c3928a36
David Costa <davidcostadev@gmail.com>
2019-07-04 22:34:28 -0300

    Update diego_bank.py

--- src/diego_bank.py
+++ src/diego_bank.py
@@ -1,2 +1,7 @@
-
 class Historico:
+    def __init__(self):
+        self.transacoes = []
+
+    def add_transacao(self, tipo, valor):
+        self.transacoes.append({'tipo': tipo, 'valor': valor})
+ 
