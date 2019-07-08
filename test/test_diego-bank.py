4189c9cba29163cfc2f4a37d01d0bc7e6f2fa67d
David Costa <davidcostadev@gmail.com>
2019-07-08 10:35:41 -0300

    Update test_diego-bank.py

--- test/test_diego-bank.py
+++ test/test_diego-bank.py
@@ -15,7 +15,7 @@ class HistoricoTest(unittest.TestCase):
 
         self.assertEqual(len(historico.transacoes), 0)
 
-        historico.add_transacao('saque', 10.45, '05/07/2019')
+        historico.add_transacao('saque', 10.45, '30/07/2019')
 
         self.assertEqual(len(historico.transacoes), 1)
 
@@ -24,23 +24,23 @@ class HistoricoTest(unittest.TestCase):
         self.assertEqual(transacao, {
             'tipo': 'saque',
             'valor': 10.45,
-            'data': datetime(2019, 7, 5, 0, 0)
+            'data': datetime(2019, 7, 30, 0, 0)
         })
     
     def test_add_transacao_validar_valores(self):
         """ Deve dar error ao adicionar valores igual ou abaixo que zero """
         historico = Historico()
         with self.assertRaisesRegex(Exception, 'Error: O valor precisa ser maior do que zero.'): 
-          historico.add_transacao('saque', 0, '05/07/2019')
+          historico.add_transacao('saque', 0, '30/07/2019')
 
         with self.assertRaisesRegex(Exception, 'Error: O valor precisa ser maior do que zero.'): 
-          historico.add_transacao('saque', -1, '05/07/2019')
+          historico.add_transacao('saque', -1, '30/07/2019')
     
     def test_add_transacao_validar_tipos(self):
         """ Deve dar error ao adicionar transacoes com tipos invalidos """
         historico = Historico()
         with self.assertRaisesRegex(Exception, 'Error: Tipo inválido. Precisa ser "saque" ou "deposito"'): 
-          historico.add_transacao('emprestimo', 100, '05/07/2019')
+          historico.add_transacao('emprestimo', 100, '30/07/2019')
 
     def test_get_transacoes(self):
       """ Deve retornar a lista de todas as transacoes """
@@ -49,17 +49,11 @@ class HistoricoTest(unittest.TestCase):
 
       self.assertEqual(len(historico.get_transacoes()), 0)
 
-      historico.add_transacao('saque', 10.45, '05/07/2019')
-      historico.add_transacao('deposito', 100, '05/07/2019')
-      historico.add_transacao('saque', 20, '05/07/2019')
+      historico.add_transacao('saque', 10.45, '30/07/2019')
+      historico.add_transacao('deposito', 100, '30/07/2019')
+      historico.add_transacao('saque', 20, '30/07/2019')
 
       self.assertEqual(len(historico.get_transacoes()), 3)
-
-
-
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
